4b677095f4afbfa90cb5f0bcd0b92478785875ce
behnam <behnamfattahzadeh90@gmail.com>
2020-02-12 19:02:09 +0330

    translate worlds

---
+++ translate world(if).py
@@ -0,0 +1,25 @@
+yourWorld=str(input(" *** hand , hat , foot , love , game , ice , boy , sun , good , big *** \n select your world and write it for means :")).lower()
+if yourWorld =="hand":
+    print("your world means is : دست")
+elif yourWorld == "hat":
+    print("your world means is : کلاه")
+elif yourWorld == "foot":
+    print("your world means is : پا")
+elif yourWorld == "game":
+    print("your world means is : بازي")
+elif yourWorld == "love":
+    print("your world means is : عشق")
+elif yourWorld == "ice":
+    print("your world means is : يخ")
+elif yourWorld == "boy":
+    print("your world means is : پسر")
+elif yourWorld == "sun":
+    print("your world means is : خورشيد")
+elif yourWorld == "good":
+    print("your world means is : خوب")
+elif yourWorld == "big":
+    print("your world means is : بزرگ")        
+else :
+        print('your world is not in list , please select your world at above list')
+        
+
